7edfeca3f9d86f008d337c45ac19b87e100eee9b
jiangjiawen <jjw@jiangjiawendeMacBook-Pro.local>
2020-02-26 10:28:27 +0800

    add go map is refrence type

---
+++ go/pointer/testMapAndSlice_test.go
@@ -0,0 +1,23 @@
+//https://dave.cheney.net/2017/04/29/there-is-no-pass-by-reference-in-go
+// map is reference type
+package testMapAndSliceTest
+
+import "testing"
+
+func fn(m map[int]int) {
+	m = make(map[int]int)
+}
+
+func fn2(m map[int]int) {
+	m[2] = 2
+}
+
+func TestMain(t *testing.T) {
+	var m map[int]int
+	fn(m)
+	t.Log(m == nil)
+	m = make(map[int]int)
+	t.Log(m == nil)
+	fn2(m)
+	t.Log(m)
+}
