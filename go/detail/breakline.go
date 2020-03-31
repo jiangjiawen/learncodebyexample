6fa07567b4756c3f172e1d2c5c719528c8ea41e1
jiangjiawen <jjw@jiangjiawendeMacBook-Pro.local>
2020-03-31 10:58:18 +0800

    add breaklinerule

---
+++ go/detail/breakline.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func False() bool {
+	return false
+}
+
+func main(){
+	switch False()
+	{
+	case true:fmt.Println("true")
+	case false:fmt.Println("false")
+	}
+	switch False(){
+	case true:fmt.Println("true")
+	case false:fmt.Println("false")
+	}
+}
