ee10077aa85d132c67e9fc0b2dda9719f31fe6d5
jiangjiawen <jjw@jiangjiawendeMacBook-Pro.local>
2020-06-08 11:52:14 +0800

    cpp value copy not reference

---
+++ cplusplus/containers/primitive.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> v{1,2,3,4,5,6,7,8};
+    vector<int> *v2 = &v;
+    v2->push_back(9);
+    cout << v.at(8) << endl;
+    cout << v[8] << endl;
+    return 0;
+}
