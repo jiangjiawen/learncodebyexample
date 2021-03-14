ad4dbb995173ffe0063ce1abe55d8c01ca51ebad
jiangjiawen <jjw@jiangjiawendeMacBook-Pro.local>
2021-03-14 23:26:52 +0800

    add last memo method example from youtube dp freecodecamp

---
+++ js/dynamicprogramming/countConstruct.js
@@ -0,0 +1,33 @@
+const countConstruct = (target, wordBank, memo = {}) => {
+  if (target in memo) return memo[target];
+  if (target === "") return 1;
+  let totalCount = 0;
+  for (let word of wordBank) {
+    if (target.indexOf(word) === 0) {
+      const numWaysForRest = countConstruct(
+        target.slice(word.length),
+        wordBank,
+        memo
+      );
+      totalCount += numWaysForRest;
+    }
+  }
+  memo[target] = totalCount;
+  return totalCount;
+};
+
+console.log(countConstruct("purple", ["puro", "p", "ur", "le", "prupl"]));
+console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+console.log(
+  countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
+);
+console.log(
+  countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+    "e",
+    "ee",
+    "eee",
+    "eeee",
+    "eeeee",
+    "eeeeeee",
+  ])
+);
