6afa2b2a016f0ed696c811e02f3034359d53ef7d
jiangjiawen <jjw@jiangjiawendeMacBook-Pro.local>
2021-03-24 22:43:20 +0800

    add new cpp example mem op

---
+++ cplusplus/interviewproblems/memmovstrcpetl.cpp
@@ -0,0 +1,69 @@
+//https://www.geeksforgeeks.org/write-memcpy/
+//https://www.tutorialspoint.com/write-your-own-memcpy-and-memmove-in-cplusplus
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+void myMemCpy(void *dest,void *src, size_t n)
+{
+    char *csrc=(char *)src;
+    char *cdest = (char *)dest;
+    for(int i=0;i<n;i++){
+        cdest[i] = csrc[i];
+    }
+}
+
+void MemcpyFunc(void *dest, void *src, size_t n){
+//    char *dataS = (char *)src;
+//    char *dataD = (char *)dest;
+   for (int i=0; i<n; i++)
+      *((char *)(dest)+i) = *((char *)(src)+i);
+}
+
+void myMemMove(void *dest, void *src, size_t n)
+{
+   // Typecast src and dest addresses to (char *)
+   char *csrc = (char *)src;
+   char *cdest = (char *)dest;
+  
+   // Create a temporary array to hold data of src
+   char *temp = new char[n];
+  
+   // Copy data from csrc[] to temp[]
+   for (int i=0; i<n; i++)
+       temp[i] = csrc[i];
+  
+   // Copy data from temp[] to cdest[]
+   for (int i=0; i<n; i++)
+       cdest[i] = temp[i];
+  
+   delete [] temp;
+}
+
+bool isNumericChar(char x) {
+   return (x >= '0' && x <= '9') ? true : false;
+}
+int myAtoi(char* str) {
+   if (*str == '\0')
+      return 0;
+   int result = 0;
+   int sign = 1;
+   int i = 0;
+   if (str[0] == '-') {
+      sign = -1;
+      i++;
+   }
+   for (; str[i] != '\0'; ++i) {
+      if (isNumericChar(str[i]) == false)
+         return 0;
+      result = result * 10 + str[i] - '0';
+   }
+   return sign * result;
+}
+int main() {
+   char string[] = "-32491841";
+   int intVal = myAtoi(string);
+   cout<<"The integer equivalent of the given string is "<<intVal;
+   return 0;
+}
